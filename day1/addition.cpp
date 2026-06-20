833386d927bb89b2662de3e3711390c3c829dc6a
Rahul yadav <rahul00200800@gmail.com>
2026-06-20 12:35:03 +0530

    Add files via upload

---
+++ day1/addition.cpp
@@ -0,0 +1,17 @@
+//program to find sum of two number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    float num1,num2,sum;
+    cout << "Enter any two numbers : ";
+    cin >> num1 >> num2;
+
+    sum = num1 + num2;
+    cout << "Sum is : " << sum << endl;
+
+    return 0;
+}
